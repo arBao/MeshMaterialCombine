e34998986a4b5eec31fb606399f01f8bfc3a9cca
arBao <286075720@qq.com>
2016-05-27 22:22:52 +0800

    first commit

---
+++ Assets/Src/CombineMeshAndMaterials.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombineMeshAndMaterials : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        
+
+        CombineMesh();
+    }
+    void CombineMesh()
+    {
+
+        MeshFilter[] mfChildren = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[mfChildren.Length];
+
+        MeshRenderer[] mrChildren = GetComponentsInChildren<MeshRenderer>();
+        Material[] materials = new Material[mrChildren.Length];
+
+        MeshRenderer mrSelf = gameObject.AddComponent<MeshRenderer>();
+        MeshFilter mfSelf = gameObject.AddComponent<MeshFilter>();
+
+        Texture2D[] textures = new Texture2D[mrChildren.Length];
+        for (int i = 0; i < mrChildren.Length; i++)
+        {
+            materials[i] = mrChildren[i].sharedMaterial;
+            Texture2D tx = materials[i].GetTexture("_MainTex") as Texture2D;
+
+            Texture2D tx2D = new Texture2D(tx.width, tx.height, TextureFormat.ARGB32, false);
+            tx2D.SetPixels(tx.GetPixels(0, 0, tx.width, tx.height));
+            tx2D.Apply();
+            textures[i] = tx2D;
+        }
+
+        Material materialNew = new Material(materials[0].shader);
+        materialNew.CopyPropertiesFromMaterial(materials[0]);
+        mrSelf.sharedMaterial = materialNew;
+
+        Texture2D texture = new Texture2D(1024, 1024);
+        materialNew.SetTexture("_MainTex", texture);
+        Rect[] rects = texture.PackTextures(textures, 10, 1024);
+
+        for (int i = 0; i < mfChildren.Length; i++)
+        {
+            if (mfChildren[i].transform == transform)
+            {
+                continue;
+            }
+            Rect rect = rects[i];
+
+            Mesh meshCombine = mfChildren[i].mesh;
+            Vector2[] uvs = new Vector2[meshCombine.uv.Length];
+            //把网格的uv根据贴图的rect刷一遍
+            for (int j = 0; j < uvs.Length; j++)
+            {
+                uvs[j].x = rect.x + meshCombine.uv[j].x * rect.width;
+                uvs[j].y = rect.y + meshCombine.uv[j].y * rect.height;
+            }
+            meshCombine.uv = uvs;
+            combine[i].mesh = meshCombine;
+            combine[i].transform = mfChildren[i].transform.localToWorldMatrix;
+            mfChildren[i].gameObject.SetActive(false);
+        }
+
+        Mesh newMesh = new Mesh();
+        newMesh.CombineMeshes(combine, true, true);//合并网格
+        mfSelf.mesh = newMesh;
+    }
+
+}
